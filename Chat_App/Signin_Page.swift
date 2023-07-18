3914817375142210ad157925e88ed68e0080f947
manthankevadiya <manthankevadiya2005@gmail.com>
2023-07-18 13:47:08 +0530

    create outlet of sign in

--- Chat_App/Signin_Page.swift
+++ Chat_App/Signin_Page.swift
@@ -3,11 +3,29 @@
 import UIKit
 
 class Signin_Page: UIViewController {
-    //feurgheg
+   
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var signInButtonOutlet: UIButton!
+    @IBOutlet weak var googleButtonOutlet: UIButton!
+    @IBOutlet weak var faceBookButtonOutlet: UIButton!
+    @IBOutlet weak var twitterButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//sdgegrefgrthrtgbhr
+
     }
     
-
+    @IBAction func twitterButtonAction(_ sender: Any) {
+        
+    }
+    @IBAction func googleButtonAction(_ sender: Any) {
+        
+    }
+    @IBAction func facebookButtonAction(_ sender: Any) {
+        
+    }
+    @IBAction func signInButtonAction(_ sender: Any) {
+        
+    }
 }
