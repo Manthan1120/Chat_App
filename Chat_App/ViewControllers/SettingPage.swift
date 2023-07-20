d354ca53e1c95072a085c633cacb1bfb15f9644b
kirtanbaldha <kirtan9397@gmail.com>
2023-07-20 11:39:16 +0530

    setting Page rearrange for Mathan

--- Chat_App/ViewControllers/SettingPage.swift
+++ Chat_App/ViewControllers/SettingPage.swift
@@ -1,16 +1,10 @@
-//
-//  SettingPage.swift
-//  Chat_App
-//
-//  Created by r86 on 19/07/23.
-//
+
 
 import UIKit
 
 class SettingPage: UIViewController {
 
-    @IBOutlet weak var tableViewForSetting: UITableView!
-    let ArrayForAllsettingName = ["Edit Account","Country","Notification","Language","Legal/About","Swich Account","Sing Out"]
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,19 +13,7 @@ class SettingPage: UIViewController {
     
 
 }
-extension SettingPage : UITableViewDelegate,UITableViewDataSource {
-  
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ArrayForAllsettingName.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableViewForSetting.dequeueReusableCell(withIdentifier: "cell") as! settingPageCell
-        cell.LableFOrSetting.text = ArrayForAllsettingName[indexPath.row]
-        return cell
-    }
+
     
     
-}
                                 
