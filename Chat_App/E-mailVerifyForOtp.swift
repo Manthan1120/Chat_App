6047f683a27d3f3f094a67e5a97a8e7bfd717ecb
kirtanbaldha <kirtan9397@gmail.com>
2023-07-21 12:59:35 +0530

    Forgot page and verify email page

---
+++ Chat_App/E-mailVerifyForOtp.swift
@@ -0,0 +1,39 @@
+//
+//  E-mailVerifyForOtp.swift
+//  Chat_App
+//
+//  Created by r85 on 21/07/23.
+//
+
+import UIKit
+import DPOTPView
+
+class E_mailVerifyForOtp: UIViewController {
+
+    @IBOutlet weak var VerifyButton: UIButton!
+    @IBOutlet weak var ViewForOt: UIView!
+    @IBOutlet weak var OtpViewOt: DPOTPView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        AllPageUi()
+        
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    func AllPageUi(){
+        ViewForOt.layer.cornerRadius = 100
+        
+        VerifyButton.layer.cornerRadius = 9
+        VerifyButton.layer.shadowColor = UIColor.systemGray4.cgColor
+        VerifyButton.layer.shadowRadius = 4.0
+        VerifyButton.layer.shadowOpacity = 4.4
+        VerifyButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        VerifyButton.layer.masksToBounds = false
+        
+    }
+    
+
+}
