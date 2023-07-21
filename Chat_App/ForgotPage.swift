0a11409e9951abacf1d6a1782cb09858ab7f4399
manthankevadiya <manthankevadiya2005@gmail.com>
2023-07-21 10:18:48 +0530

    changing in forgot page

--- Chat_App/ForgotPage.swift
+++ Chat_App/ForgotPage.swift
@@ -25,14 +25,14 @@ class ForgotPage: UIViewController {
             
             TextFieldeOt.layer.backgroundColor = UIColor.white.cgColor
             TextFieldeOt.layer.cornerRadius = 9
-            TextFieldeOt.layer.shadowColor = UIColor.systemGray3.cgColor
+            TextFieldeOt.layer.shadowColor = UIColor.systemGray4.cgColor
             TextFieldeOt.layer.shadowRadius = 4.0
             TextFieldeOt.layer.shadowOpacity = 4.4
             TextFieldeOt.layer.shadowOffset = CGSize(width: 4, height: 4)
             TextFieldeOt.layer.masksToBounds = false
             
             SendButtonOt.layer.cornerRadius = 9
-            SendButtonOt.layer.shadowColor = UIColor.systemGray3.cgColor
+            SendButtonOt.layer.shadowColor = UIColor.systemGray4.cgColor
             SendButtonOt.layer.shadowRadius = 4.0
             SendButtonOt.layer.shadowOpacity = 4.4
             SendButtonOt.layer.shadowOffset = CGSize(width: 4, height: 4)
@@ -40,7 +40,10 @@ class ForgotPage: UIViewController {
             
         }
 
-       
+    @IBAction func backButtonAction(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
 
     }
 
