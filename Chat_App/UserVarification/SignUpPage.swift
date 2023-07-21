7e713d5408a7a046eb36de84564ef9f48af4128c
manthankevadiya <manthankevadiya2005@gmail.com>
2023-07-21 12:59:58 +0530

    Done(Firebase Signin & SignUp With Showing Alert)

--- Chat_App/UserVarification/SignUpPage.swift
+++ Chat_App/UserVarification/SignUpPage.swift
@@ -1,21 +1,27 @@
 
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseCore
+import FirebaseFirestore
 
 class SignUpPage: UIViewController {
     
-    @IBOutlet weak var emailOutlate: UITextField!
-    @IBOutlet weak var passwordOutlate:UITextField!
-    @IBOutlet weak var confirmPasswordOutlate: UITextField!
+    @IBOutlet weak var userOutlet: UITextField!
+    @IBOutlet weak var emailOutlet:UITextField!
+    @IBOutlet weak var passwordOutlate: UITextField!
     @IBOutlet weak var singUpButtonOutlate: UIButton!
     @IBOutlet weak var googleOutlate: UIButton!
     @IBOutlet weak var facbookOutlate: UIButton!
     @IBOutlet weak var twiterOutlate: UIButton!
     
+    var fir: Firestore!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         set()
+        fir = Firestore.firestore()
     }
     
     func set() {
@@ -47,14 +53,6 @@ class SignUpPage: UIViewController {
         singUpButtonOutlate.layer.shadowOffset = CGSize(width: 4, height: 4)
         singUpButtonOutlate.layer.masksToBounds = false
         
-        confirmPasswordOutlate.layer.backgroundColor = UIColor.white.cgColor
-        confirmPasswordOutlate.layer.cornerRadius = 9
-        confirmPasswordOutlate.layer.shadowColor = UIColor.systemGray3.cgColor
-        confirmPasswordOutlate.layer.shadowRadius = 4.0
-        confirmPasswordOutlate.layer.shadowOpacity = 4.4
-        confirmPasswordOutlate.layer.shadowOffset = CGSize(width: 4, height: 4)
-        confirmPasswordOutlate.layer.masksToBounds = false
-        
         passwordOutlate.layer.backgroundColor = UIColor.white.cgColor
         passwordOutlate.layer.cornerRadius = 9
         passwordOutlate.layer.shadowColor = UIColor.systemGray3.cgColor
@@ -63,26 +61,61 @@ class SignUpPage: UIViewController {
         passwordOutlate.layer.shadowOffset = CGSize(width: 4, height: 4)
         passwordOutlate.layer.masksToBounds = false
         
-        emailOutlate.layer.backgroundColor = UIColor.white.cgColor
-        emailOutlate.layer.cornerRadius = 9
-        emailOutlate.layer.shadowColor = UIColor.systemGray3.cgColor
-        emailOutlate.layer.shadowRadius = 4.0
-        emailOutlate.layer.shadowOpacity = 4.4
-        emailOutlate.layer.shadowOffset = CGSize(width: 4, height: 4)
-        emailOutlate.layer.masksToBounds = false
+        emailOutlet.layer.backgroundColor = UIColor.white.cgColor
+        emailOutlet.layer.cornerRadius = 9
+        emailOutlet.layer.shadowColor = UIColor.systemGray3.cgColor
+        emailOutlet.layer.shadowRadius = 4.0
+        emailOutlet.layer.shadowOpacity = 4.4
+        emailOutlet.layer.shadowOffset = CGSize(width: 4, height: 4)
+        emailOutlet.layer.masksToBounds = false
+        
+        userOutlet.layer.backgroundColor = UIColor.white.cgColor
+        userOutlet.layer.cornerRadius = 9
+        userOutlet.layer.shadowColor = UIColor.systemGray3.cgColor
+        userOutlet.layer.shadowRadius = 4.0
+        userOutlet.layer.shadowOpacity = 4.4
+        userOutlet.layer.shadowOffset = CGSize(width: 4, height: 4)
+        userOutlet.layer.masksToBounds = false
     }
     
     @IBAction func signInButtonAction(_ sender: Any) {
-        let navigation = storyboard?.instantiateViewController(withIdentifier: "TabBar") as! TabBar
-        navigationController?.pushViewController(navigation, animated: true)
+        fireBaseAuth()
+    }
+    
+    func fireBaseAuth() {
+        Auth.auth().createUser(withEmail: userOutlet.text!, password: emailOutlet.text!) { [self]authDataResult, error in
+            if error == nil {
+                let uid = authDataResult?.user.uid
+                self.fir.collection("User").document(uid!).setData(["userName":self.userOutlet.text!,"email":self.emailOutlet.text!,"password":self.passwordOutlate.text!])
+                navigate()
+                
+            }else{
+                showAlert(title: error!.localizedDescription)
+            }
+        }
     }
     
     @IBAction func googleButtonAction(_ sender: Any) {
+        
     }
     
     @IBAction func facbookButtonAction(_ sender: Any) {
+        
     }
+    
     @IBAction func twiterButtonAction(_ sender: Any) {
+        
+    }
+    
+    func showAlert(title:String) {
+        let alert = UIAlertController(title: "Error!!", message: title, preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "Ok", style: .default, handler: nil))
+        present(alert, animated:true, completion: nil)
+    }
+    
+    func navigate() {
+        let navigation = storyboard?.instantiateViewController(withIdentifier: "TabBar") as! TabBar
+        navigationController?.pushViewController(navigation, animated: true)
     }
     
 }
