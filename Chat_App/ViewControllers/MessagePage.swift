7be5a32a9a5225695a20765d2744704bfff465d6
rajpanchani <panchaniraj9@gmail.com>
2023-08-03 15:00:54 +0530

    text file and send button !

--- Chat_App/ViewControllers/MessagePage.swift
+++ Chat_App/ViewControllers/MessagePage.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MessagePage: UIViewController {
 
+    @IBOutlet weak var sendButtonOtlet: UIButton!
+    @IBOutlet weak var textFiledForUser: UITextField!
     @IBOutlet weak var MassegeUserImage: UIImageView!
     
     override func viewDidLoad() {
@@ -21,8 +23,23 @@ class MessagePage: UIViewController {
     @IBAction func CloseButton(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
+    
     func Allui() {
-        MassegeUserImage.layer.cornerRadius = 20
+     
+        sendButtonOtlet.layer.cornerRadius = 15
+        sendButtonOtlet.layer.shadowColor = UIColor.systemGray3.cgColor
+        sendButtonOtlet.layer.shadowRadius = 4.0
+        sendButtonOtlet.layer.shadowOpacity = 4.4
+        sendButtonOtlet.layer.shadowOffset = CGSize(width: 4, height: 4)
+        sendButtonOtlet.layer.masksToBounds = false
+    
+        textFiledForUser.layer.cornerRadius = 15
+        textFiledForUser.layer.shadowColor = UIColor.systemGray3.cgColor
+        textFiledForUser.layer.shadowRadius = 4.0
+        textFiledForUser.layer.shadowOpacity = 4.4
+        textFiledForUser.layer.shadowOffset = CGSize(width: 4, height: 4)
+        textFiledForUser.layer.masksToBounds = false
+        
     }
 
    
