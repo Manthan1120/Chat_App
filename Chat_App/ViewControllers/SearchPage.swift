11c63de226e83cb6aa6e396cdecac2ddb16d16e5
rajpanchani <panchaniraj9@gmail.com>
2023-08-09 15:08:27 +0530

    now user can search user in search page

--- Chat_App/ViewControllers/SearchPage.swift
+++ Chat_App/ViewControllers/SearchPage.swift
@@ -22,11 +22,11 @@ struct UserData {
 
 class SearchPage: UIViewController {
     
-    @IBOutlet weak var searchButton: UIButton!
-    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tabelView: UITableView!
     
     var arrayOfUsersData = [UserData]()
+    var nullArr = [UserData]()
     var ref : DatabaseReference!
     var colRef : CollectionReference!
     var fir : Firestore!
@@ -36,28 +36,8 @@ class SearchPage: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getFirData()
-        setUI()
-        
     }
-    
-    func setUI(){
-        searchTextField.layer.backgroundColor = UIColor.white.cgColor
-        searchTextField.layer.cornerRadius = 9
-        searchTextField.layer.shadowColor = UIColor.systemGray3.cgColor
-        searchTextField.layer.shadowRadius = 4.0
-        searchTextField.layer.shadowOpacity = 4.4
-        searchTextField.layer.shadowOffset = CGSize(width: 4, height: 4)
-        searchTextField.layer.masksToBounds = false
-        
-        searchButton.layer.backgroundColor = UIColor.white.cgColor
-        searchButton.layer.cornerRadius = 9
-        searchButton.layer.shadowColor = UIColor.systemGray3.cgColor
-        searchButton.layer.shadowRadius = 4.0
-        searchButton.layer.shadowOpacity = 4.4
-        searchButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-        searchButton.layer.masksToBounds = false
-    }
-    
+
     func getFirData(){
         
         colRef = Firestore.firestore().collection("UserProfile")
@@ -72,6 +52,7 @@ class SearchPage: UIViewController {
                     }
                     return nil
                 }
+                nullArr = arrayOfUsersData
                 tabelView.reloadData()
                 print(arrayOfUsersData)
             }
@@ -82,13 +63,13 @@ class SearchPage: UIViewController {
 extension SearchPage: UITableViewDelegate,UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayOfUsersData.count
+        return nullArr.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! SearchTableViewCell
-        cell.userNameLabel.text! = arrayOfUsersData[indexPath.row].Username
-        cell.userImage.sd_setImage(with: URL(string: arrayOfUsersData[indexPath.row].ProfileImageUrl))
+        cell.userNameLabel.text! = nullArr[indexPath.row].Username
+        cell.userImage.sd_setImage(with: URL(string: nullArr[indexPath.row].ProfileImageUrl))
         return cell
     }
     
@@ -97,4 +78,17 @@ extension SearchPage: UITableViewDelegate,UITableViewDataSource{
         
     }
     
+}
+
+extension SearchPage: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text!.isEmpty{
+            nullArr = arrayOfUsersData
+            print(nullArr)
+        }else{
+            nullArr = arrayOfUsersData.filter{$0.Username.hasPrefix(searchBar.text!)}
+            print(nullArr)
+        }
+        tabelView.reloadData()
+    }
 }
