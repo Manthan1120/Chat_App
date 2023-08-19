55b4200292b9d0ecef2f74815cf6b917cd8fab7a
kirtanbaldha <kirtan9397@gmail.com>
2023-08-19 12:37:59 +0530

    feedback page running .

---
+++ Chat_App/ViewControllers/FeedBackPage.swift
@@ -0,0 +1,19 @@
+//
+//  FeedBackPage.swift
+//  Chat_App
+//
+//  Created by r85 on 19/08/23.
+//
+
+import UIKit
+
+class FeedBackPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
